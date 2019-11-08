41229b2083ea04b1eeb65b9634b274359d2a24e6
Tony Tong <ttong@pro-ai.org>
2019-11-08 15:26:12 -0800

    Remove model_name assertion

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -148,8 +148,6 @@ def finetune(sess,
     See that file for parameter definitions.
     """
 
-    # assert model_name not in ['774M', '1558M'], "Currently, modern GPUs cannot finetune the 774M GPT-2 model or larger."
-
     SAMPLE_DIR = 'samples'
 
     checkpoint_path = os.path.join(checkpoint_dir, run_name)
